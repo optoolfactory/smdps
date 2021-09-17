ccc1c52adf283396eedcc94319b88a498fd20ce4
multikyd <72329880+multikyd@users.noreply.github.com>
2021-09-17 23:33:34 +0900

    openpilot v0.8.2

--- panda/board/safety/safety_defaults.h
+++ panda/board/safety/safety_defaults.h
@@ -1,50 +1,40 @@
 int openpilot_live = 0;
-int mdps_bus = -1, scc_bus = -1;
+int mdps_bus = -1;
 bool mdps_spoof_active = false;
-bool scc_spoof_active = false;
 
 void mdps_spoof_speed(CAN_FIFOMailBox_TypeDef *to_fwd){
   bool mph_speed = GET_BYTE(to_fwd, 2) & 0x2;
   int enable_speed = mph_speed ? 38 : 60;
   int speed = (GET_BYTE(to_fwd, 1) | (GET_BYTE(to_fwd, 2) & 0x1) << 8) * 0.5;
+  puts("  car speed: ");puth2(speed); puts("\r");
+
   if (speed < enable_speed) {
     to_fwd->RDLR &= 0xFFFE00FF;
     to_fwd->RDLR |= (enable_speed * 2) << 8;
   }
 };
-void scc_spoof_address(CAN_FIFOMailBox_TypeDef *to_fwd){
-  // int addr = GET_ADDR(to_fwd);
-  to_fwd->RIR += 0x1400000; // addr + 10
-}
 
 int default_rx_hook(CAN_FIFOMailBox_TypeDef *to_push) {
   int bus = GET_BUS(to_push);
   int addr = GET_ADDR(to_push);
   if (addr == 524 && bus != mdps_bus && mdps_bus != 0) {
     mdps_bus = bus;
+    puts("  MDPS on bus ");puth2(bus);puts("\n");
   }
-  if (addr == 1057) {
-    if (bus == -1) {
-      scc_bus = bus;
-    }
-  } else if (bus == 0 && scc_bus == 1) {
-    // scc_spoof_active = true;
+  if (!mdps_spoof_active && (mdps_bus == 1 || mdps_bus == 2)) {
+    mdps_spoof_active = true;
+    puts("  MDPS speed spoofing enabled\n");
   }
-  if (!mdps_spoof_active && mdps_bus == 2) {mdps_spoof_active = true;}
   // TODO: Openpilot send de/activation cmd
   // if (bus == 0 && addr == 0x2AB) {
   //   openpilot_live = 2;
   //   if (GET_BYTE(to_push, 0) & 0x7) {
   //     mdps_spoof_active = true;
+  //     puts("  Openpilot enabled mdps spoofing\n")
   //   }
   //   else if (GET_BYTE(to_push, 0) & 0x3){
   //     mdps_spoof_active = false;
-  //   }
-  //   if (GET_BYTE(to_push, 1) & 0x7) {
-  //     scc_spoof_active = true;
-  //   }
-  //   else if (GET_BYTE(to_push, 1) & 0x3){
-  //     scc_spoof_active = false;
+  //     puts("  Openpilot disabled mdps spoofing\n")
   //   }
   // }
   return true;
@@ -56,6 +46,7 @@ static void nooutput_init(int16_t param) {
   UNUSED(param);
   controls_allowed = false;
   relay_malfunction_reset();
+  puts("  safety mode set to NOOUTPUT\n");
 }
 
 static int nooutput_tx_hook(CAN_FIFOMailBox_TypeDef *to_send) {
@@ -73,22 +64,14 @@ static int nooutput_tx_lin_hook(int lin_num, uint8_t *data, int len) {
 static int default_fwd_hook(int bus_num, CAN_FIFOMailBox_TypeDef *to_fwd) {
   int addr = GET_ADDR(to_fwd);
   int bus_fwd = -1;
-  bool scc_address = addr == 1056 || addr == 1057 || addr == 1290 || addr == 905 || addr == 1186;
 
   if (bus_num == 0) {
-    bool forward_to_bus1 = scc_bus == 1 && !scc_address;
-    bus_fwd = forward_to_bus1 ? 12 : 2;
+    bus_fwd = mdps_bus;
     if (addr == 1265 && mdps_spoof_active) {
       mdps_spoof_speed(to_fwd);
     }
   }
-  if (bus_num == 1) {
-    bus_fwd = 20;
-    if (scc_address && scc_spoof_active) {
-      scc_spoof_address(to_fwd);
-    }
-  }
-  if (bus_num == 2) {
+  else if (bus_num == mdps_bus) {
     bus_fwd = 0;
   }
   return bus_fwd;
@@ -108,14 +91,16 @@ static void alloutput_init(int16_t param) {
   UNUSED(param);
   controls_allowed = true;
   relay_malfunction_reset();
+  puts("  safety mode set to ALLOUTPUT\n");
 }
 
 static int alloutput_tx_hook(CAN_FIFOMailBox_TypeDef *to_send) {
   int addr = GET_ADDR(to_send);
   if (addr == 0x2AA) {
-    to_send->RDLR &= mdps_spoof_active ? 0x7 : 0x3 | (scc_spoof_active ? 0x7 : 0x3) << 8;
+    to_send->RDLR &= mdps_spoof_active ? 0x7 : 0x3;
     if (openpilot_live < 1) {
       // mdps_spoof_active = false;
+      // puts("  Openpilot not live, mdps speed spoofing disabled\n")
     } else {
       openpilot_live--;
     }
