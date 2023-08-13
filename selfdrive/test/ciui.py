2786b2ebefff269b42ddd14842386d6fdb218170
Comma Device <device@comma.ai>
2023-08-13 13:06:15 -0700

    looks good

--- selfdrive/test/ciui.py
+++ selfdrive/test/ciui.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
+import signal
 import subprocess
+
+signal.signal(signal.SIGINT, signal.SIG_DFL)
+signal.signal(signal.SIGTERM, signal.SIG_DFL)
 
 from PyQt5.QtCore import QTimer  # pylint: disable=no-name-in-module, import-error
 from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel # pylint: disable=no-name-in-module, import-error
@@ -10,14 +14,17 @@ class Window(QWidget):
     super(Window, self).__init__(parent)
 
     layout = QVBoxLayout()
-    layout.addStretch(1)
     self.setLayout(layout)
+
+    self.l = QLabel("jenkins runner")
+    layout.addWidget(self.l)
+    layout.addStretch(1)
+    layout.setContentsMargins(20, 20, 20, 20)
 
     cmds = [
       "cat /etc/hostname",
-      "uptime",
-      "cat /VERSION",
-      "cat /BUILD",
+      "echo AGNOS v$(cat /VERSION)",
+      "uptime -p",
     ]
     self.labels = {}
     for c in cmds:
@@ -29,13 +36,14 @@ class Window(QWidget):
         color: white;
         font-size: 55px;
         background-color: black;
+        font-family: "JetBrains Mono";
       }
     """)
 
     self.timer = QTimer()
     self.timer.timeout.connect(self.update)
     self.timer.start(10 * 1000)
-    #self.update()
+    self.update()
 
   def update(self):
     for cmd, label in self.labels.items():
